d998e5339b71af9b845e16442ed401b240328368
tasty_fan14 <tastyfan14@gmail.com>
2026-08-12 08:04:39 +1000

    fix: improved code structure

--- src/entities/form/ui/PlanRadioCard.tsx
+++ src/entities/form/ui/PlanRadioCard.tsx
@@ -6,9 +6,11 @@ export default function PlanRadioCard({ variant, id, icon: Icon, title, price, b
     return (
         <div className={clsx(cls['plan'], cls[id])}>
             <Icon />
-            <h2 className={cls['plan__title']}>{title}</h2>
-            <h3 className={cls['plan__price']}>${price}{variant === 'year' ? '/yr' : '/mo'}</h3>
-            {variant === 'year' ? <h4 className={cls['plan__bonus']}>{bonus}</h4> : null}
+            <div className={cls['plan__overview']}>
+                <h2 className={cls['plan__overview--title']}>{title}</h2>
+                <h3 className={cls['plan__overview--price']}>${price}{variant === 'year' ? '/yr' : '/mo'}</h3>
+                {variant === 'year' ? <h4 className={cls['plan__overview--bonus']}>{bonus}</h4> : null}
+            </div>
         </div>
     )
 }
