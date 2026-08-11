5e1e664501502d32351301a7fa28cbb3942bc9b9
tasty_fan14 <tastyfan14@gmail.com>
2026-08-11 11:39:38 +1000

    chore: add input

---
+++ src/shared/ui/field/input/Input.tsx
@@ -0,0 +1,18 @@
+import type { InputProps } from './types'
+import cls from './Input.module.scss'
+import clsx from 'clsx'
+
+export default function Input({ rightEl, leftEl, className, ...props }: InputProps) {
+    return (
+        <div>
+            {rightEl}
+
+            <input
+            className={clsx(cls['input'], className && className)}
+            {...props}
+            />
+
+            {leftEl}
+        </div>
+    )
+}
---
+++ src/shared/ui/field/input/types.ts
@@ -0,0 +1,6 @@
+export type InputProps = {
+    rightEl?: React.ReactNode
+    leftEl?: React.ReactNode
+
+    className?: string
+} & React.ComponentPropsWithoutRef<'input'>
