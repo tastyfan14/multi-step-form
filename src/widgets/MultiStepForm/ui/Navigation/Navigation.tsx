fd69bf1c8f5d83d15ee25aa811572df2d17e183e
tasty_fan14 <tastyfan14@gmail.com>
2026-08-15 23:45:25 +1000

    feat: add navigation panel

---
+++ src/widgets/MultiStepForm/ui/Navigation/Navigation.tsx
@@ -0,0 +1,44 @@
+import Button from '@/shared/ui/button'
+import cls from './Navigation.module.scss'
+import type { NavigationProps } from '../../model/types'
+import { FIELD_STEPS } from '@/entities/multi-step-form/model/config'
+
+export function Navigation({ prevStep, nextStep, currentStep }: NavigationProps) {
+    return (
+        <div className={cls['navigation']}>
+            {currentStep !== 1 && (
+                    <Button
+                    type='button'
+                    variant='additional'
+                    className={cls['navigation__prev']}
+                    onClick={prevStep}
+                    >
+                        Go Back
+                    </Button>
+            )}
+
+            {currentStep !== FIELD_STEPS.length
+                ? (
+                    <Button
+                    type='button'
+                    variant='primary'
+                    className={cls['navigation__next']}
+                    onClick={nextStep}
+                    >
+                        Next Step
+                    </Button>
+                )
+                : (
+                    <Button
+                    type='submit'
+                    variant='secondary'
+                    className={cls['navigation__next']}
+                    onClick={nextStep}
+                    >
+                        Confirm
+                    </Button>
+                )
+            }
+        </div>
+    )
+}
