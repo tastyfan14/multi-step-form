b01221cb4c147a0dff70f6ee235376833246543e
tasty_fan14 <tastyfan14@gmail.com>
2026-08-15 23:33:17 +1000

    feat: add types for form widgets

---
+++ src/widgets/MultiStepForm/model/types.ts
@@ -0,0 +1,32 @@
+// Steps
+
+export type StepItem = {
+    step: number
+    title: string
+}
+
+export type StepProps = {
+    active: boolean
+} & StepItem
+
+export type StepsProps = {
+    steps: StepItem[]
+    currentStep: number
+    className?: string
+}
+
+// Navigation
+
+export type NavigationProps = {
+    prevStep: () => void
+    nextStep: () => void
+    currentStep: number
+}
+
+// Step content
+
+export type StepContentProps = {
+    title: string
+    desc: string
+    children: React.ReactNode
+}
