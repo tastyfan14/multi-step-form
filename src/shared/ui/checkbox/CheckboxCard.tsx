72836daf3876e626a4c189c43ef65e8f98e15860
tasty_fan14 <tastyfan14@gmail.com>
2026-08-16 12:47:10 +1000

    feat: add card

---
+++ src/shared/ui/checkbox/CheckboxCard.tsx
@@ -0,0 +1,23 @@
+import clsx from 'clsx'
+import cls from './CheckboxCard.module.scss'
+import type { CheckboxCardProps } from './types'
+import Label from '@/shared/ui/label'
+import Checkbox from './Checkbox'
+
+
+export default function CheckboxCard({ id, children, className, checked, onChange, ...props }: CheckboxCardProps) {
+    return (
+        <Label variant='in' htmlFor={id} className={clsx(cls['checkbox-card'], className && className)}>
+            <Checkbox
+            id={id}
+            variant='card'
+            className={cls['checkbox-card__checkbox']}
+            checked={checked}
+            onChange={(e) => onChange(e.target.checked)}
+            {...props}
+            />
+
+            {children}
+        </Label>
+    )
+}
