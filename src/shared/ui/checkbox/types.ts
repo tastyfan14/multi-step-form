b792367f616998a806c1e822bd4880f78f62fa81
tasty_fan14 <tastyfan14@gmail.com>
2026-08-16 12:45:49 +1000

    feat: add checkboxcard type

--- src/shared/ui/checkbox/types.ts
+++ src/shared/ui/checkbox/types.ts
@@ -1,4 +1,12 @@
 export type CheckboxProps = {
     className?: string
     children?: React.ReactNode
+    variant: 'independent' | 'card'
+} & React.ComponentPropsWithoutRef<'input'>
+
+export type CheckboxCardProps = {
+    children: React.ReactNode
+    className?: string
+    checked: boolean
+    onChange: (checked: boolean) => void
 } & React.ComponentPropsWithoutRef<'input'>
