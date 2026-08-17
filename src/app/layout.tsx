405c99a170034a7fc315bddb6af7696a5d7801f3
tasty_fan14 <tastyfan14@gmail.com>
2026-08-18 08:32:47 +1000

    feat: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,10 +10,29 @@ const UbuntuFont = Ubuntu({
 })
 
 export const metadata: Metadata = {
-    title: 'Multi-step form',
+    title: 'Multi-Step Form',
     description: 'Challenge by Frontend Mentor. Coded by @tasty_fan14.',
     icons: {
         icon: 'favicon-32x32.png'
+    },
+    keywords: ['Next.js', 'React', 'TypeScript', 'SCSS'],
+    openGraph: {
+        title: 'Multi-Step Form',
+        description: 'Challenge by Frontend Mentor. Coded by @tasty_fan14.',
+        type: 'website',
+        images: [
+            {
+                url: '/preview.jpg',
+                width: 1440,
+                height: 1056,
+            },
+        ],
+    },
+    twitter: {
+        title: 'Multi-Step Form',
+        description: 'Challenge by Frontend Mentor. Coded by @tasty_fan14.',
+        card: 'summary_large_image',
+        images: '/preview.jpg',
     }
 }
 
