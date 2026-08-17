345c3babececdbbe901f4164e11c4acbcb4dc0fa
tasty_fan14 <tastyfan14@gmail.com>
2026-08-17 11:40:36 +1000

    fix: add properties for new types

--- src/shared/ui/radio/RadioGroup.tsx
+++ src/shared/ui/radio/RadioGroup.tsx
@@ -2,7 +2,7 @@ import RadioCard from './RadioCard'
 import type { RadioGroupProps } from './types'
 import cls from './RadioGroup.module.scss'
 
-export default function RadioGroup<T extends({ id: string })>({ name, options, renderCard }: RadioGroupProps<T>) {
+export default function RadioGroup<T extends({ id: string })>({ name, options, renderCard, value, onChange, ...props }: RadioGroupProps<T>) {
     return (
         <div className={cls['radio-group']}>
             {options.map((option) => {
@@ -12,6 +12,9 @@ export default function RadioGroup<T extends({ id: string })>({ name, options, r
                     key={option.id}
                     id={id}
                     name={name}
+                    checked={value === option.id}
+                    onChange={() => onChange?.(option.id)}
+                    {...props}
                     >
                         {renderCard(option)}
                     </RadioCard>
