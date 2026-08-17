d10907c6b9584c7b4feb859b19e6c7d0e3c213cb
tasty_fan14 <tastyfan14@gmail.com>
2026-08-17 11:32:48 +1000

    fix: fragmentation of use cases

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -2,14 +2,29 @@ import { ButtonProps } from './types'
 import cls from './Button.module.scss'
 import clsx from 'clsx'
 import Spinner from '@/shared/ui/spinner'
+import Link from 'next/link'
 
-export default function Button({ children, variant = 'primary', className, fullWidth, loading, ...props }: ButtonProps) {
+export default function Button(props: ButtonProps) {
+    if (props.as === 'link') {
+        const { as: _as, variant, className, href, children, ...linkProps } = props
+
+        return (
+            <Link
+            className={clsx(cls[variant], className, cls['link'])}
+            href={href}
+            {...linkProps}>
+                {children}
+            </Link>
+        )
+    }
+    const { as: _as, variant, className, fullWidth, loading, children, ...btnProps } = props
+
     return (
         <button
-        className={clsx(cls[variant], fullWidth && cls.full, className && className)}
+        className={clsx(cls[variant], fullWidth && cls.full, className)}
         aria-busy={loading}
         disabled={loading}
-        {...props}
+        {...btnProps}
         >
             {loading ? <Spinner /> : children}
         </button>
