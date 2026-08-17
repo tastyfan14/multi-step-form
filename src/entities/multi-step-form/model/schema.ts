c311638cd685bfe908df231ee1617818bb7bf0b6
tasty_fan14 <tastyfan14@gmail.com>
2026-08-17 11:29:23 +1000

    fix: update input validation

--- src/entities/multi-step-form/model/schema.ts
+++ src/entities/multi-step-form/model/schema.ts
@@ -1,26 +1,31 @@
+import { isValidPhoneNumber } from 'libphonenumber-js'
 import z from 'zod'
 
 export const FormSchema = z.object({
     name: z
         .string()
         .trim()
-        .regex(/^[\p{L}]+(?:[ '-][\p{L}]+)+$/u, 'Name is required'),
+        .min(1, 'Name is required')
+        .max(50, 'Name must be less than 50 characters')
+        .regex(/^[\p{L}]+(?:[ '-][\p{L}]+)+$/u, 'Enter your first and last name (e.g. Stephen King)'),
     email: z
-        .string()
+        .email('Enter a valid email address')
         .trim()
-        .min(7, 'Email is required')
-        .email(),
+        .max(254, 'Email must be less than 254 characters'),
     phone: z
         .string()
         .trim()
-        .min(5),
+        .min(1, 'Phone number is required')
+        .refine(
+            (phone) => isValidPhoneNumber(phone), {
+                message: 'Enter a valid phone number',
+            }
+        ),
 
     plan: z
         .enum(['plan-arcade', 'plan-advanced', 'plan-pro'])
         .optional()
-        .refine((plan) => plan !== undefined, {
-            message: 'Plan is required',
-        }),
+        .refine((plan) => plan !== undefined),
     billingPeriod: z
         .enum(['month', 'year']),
 
